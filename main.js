a93e013daed34b8a0a46207b53a6a91e565d3cb6
juniorgural <137784826+juniorgural@users.noreply.github.com>
2025-06-06 09:21:33 -0300

    parte 6 iniciado

--- main.js
+++ main.js
@@ -16,9 +16,13 @@ botoes[i].onclick = fuction () {
 
 const contadores =  document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
+const tempoObjetivo2 = new Date("2026-01-10T00:00:00");
+const tempoObjetivo3 = new Date("2026-03-25T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-30T00:00:00");
 
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-  contadores[0].textContent =calculaTempo(tempoObjetivo1);
+    contadores[0].textContent =calculaTempo(tempoObjetivo1);
 
   function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
